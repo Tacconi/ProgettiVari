d7832e696c182656de7b709be196d4c2bf5008bc
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2015-11-07 12:17:46 +0100

    Create Classi e aggiunti File

--- TestCodiceSemaforo/StartSequence/StartSequence.cs
+++ TestCodiceSemaforo/StartSequence/StartSequence.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace StartSequence
 {
+    public class SequenceEventArgs : EventArgs
+    {
+        public LightColor Light { get; set; } //Luce
+        public SoundType Sound { get; set; } //Suono
+
+    }
+
     public class StartSequence
     {
+        public event EventHandler<SequenceEventArgs> SequenceChange; //evento per cambio colore e suono
+
         public StartSequence()
-        { }
-       public  StartSequence(int stepIntervall)
-        {}
+        {
+
+        }
+
+        public StartSequence(int stepIntervall)
+        {
+
+        }
+        public void Start()
+        {
+
+        }
+        public void Abort()
+        {
+
+        }
+
+
+
     }
 }
