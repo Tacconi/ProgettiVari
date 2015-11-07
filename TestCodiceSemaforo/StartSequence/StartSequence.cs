edfeb9d54f27a4710a969228d348b56b90d737f9
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2015-11-07 12:38:30 +0100

    Scritto codice in start sequence

--- TestCodiceSemaforo/StartSequence/StartSequence.cs
+++ TestCodiceSemaforo/StartSequence/StartSequence.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StartSequence
 {
-    public class SequenceEventArgs : EventArgs
+    public class SequenceChangeEventArgs : EventArgs
     {
         public LightColor Light { get; set; } //Luce
         public SoundType Sound { get; set; } //Suono
@@ -15,7 +15,7 @@ namespace StartSequence
 
     public class StartSequence
     {
-        public event EventHandler<SequenceEventArgs> SequenceChange; //evento per cambio colore e suono
+        public event EventHandler<SequenceChangeEventArgs> SequenceChange; //evento per cambio colore e suono
 
         public StartSequence()
         {
@@ -28,8 +28,27 @@ namespace StartSequence
         }
         public void Start()
         {
+            TimeSpan Change = new TimeSpan(0, 0, 5);
+            OnSequenceChange(LightColor.Red, SoundType.Change); // 5 secondi
+            //subito dopo
+            OnSequenceChange(LightColor.Yellow, SoundType.Change); //5secondi
+            // subito dopo 
+            OnSequenceChange(LightColor.Sec5, SoundType.Change); //1 secondo
+            OnSequenceChange(LightColor.Sec4, SoundType.Change); //1 secondo
+            OnSequenceChange(LightColor.Sec3, SoundType.Change); //1 secondo
+            OnSequenceChange(LightColor.Sec2, SoundType.Change); //1 secondo
+            OnSequenceChange(LightColor.Sec1, SoundType.Change); //1 secondo
+            //subito dopo 
+            OnSequenceChange(LightColor.Green, SoundType.Start); //fissa
 
         }
+
+        private void OnSequenceChange(LightColor light, SoundType sound)
+        {
+            if (SequenceChange != null)
+                SequenceChange(this, new SequenceChangeEventArgs() { Light = light, Sound = sound });
+        }
+
         public void Abort()
         {
 
